1ac39c4102f12e07b818832e9c77bdb7ffac76c2
Scott Taylor <scott@railsnewbie.com>
2008-08-31 15:42:13 -0400

    * Add drb server

---
+++ lib/drb_server.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + "/../config/environment"
+
+require "colored"
+require 'drb'
+here = 'druby://127.0.0.1:10000'
+connection = User.connection
+connection.extend DRb::DRbUndumped
+
+def connection.select(sql, name = nil)
+  puts "* Executing SQL: " + "#{sql}".red
+  @connection.query_with_result = true
+  result = execute(sql, name)
+  rows = result.all_hashes
+  result.free
+  rows.extend DRb::DRbUndumped
+  rows
+end
+
+DRb.start_service here, connection
+DRb.thread.join
+puts '* Waiting for connections'
